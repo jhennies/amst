c7177c94642bc68d45eb664f12d050129f953a05
jhennies <julianhennies@hotmail.de>
2019-11-15 12:00:24 +0100

    some testing

--- example_hela_dataset.py
+++ example_hela_dataset.py
@@ -3,8 +3,8 @@ from amst_main import amst_align
 import numpy as np
 
 raw_folder = '/data/datasets/20140801_hela-wt_xy5z8nm_as_full_8bit/raw_8bit'
-pre_alignment_folder = '/data/datasets/20140801_hela-wt_xy5z8nm_as_full_8bit/template_match_aligned'
-target_folder = '/data/datasets/20140801_hela-wt_xy5z8nm_as_full_8bit/amst_aligned_new'
+pre_alignment_folder = '/data/datasets/20140801_hela-wt_xy5z8nm_as_full_8bit/tm_ali_refined'
+target_folder = '/data/datasets/20140801_hela-wt_xy5z8nm_as_full_8bit/amst_pre_imod'
 median_radius = 7
 n_workers = 12
 
@@ -14,8 +14,8 @@ amst_align(
     target_folder=target_folder,
     median_radius=median_radius,
     n_workers=n_workers,
-    source_range=np.s_[:200],
-    with_sift=False,
+    source_range=np.s_[:700],
+    with_sift=True,
     sift_params=dict(
         shift_only=True,
         subpixel_displacement=False,
