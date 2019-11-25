1a3ef9ffd0d9e5a2737d044659a07512458b5bcc
jhennies <julianhennies@hotmail.de>
2019-11-25 11:53:53 +0100

    Putting the amst call into a __name__ == '__main__' block, to also work in Windows

--- example_usage.py
+++ example_usage.py
@@ -9,9 +9,12 @@ target_folder = '/where/to/save/the/results'
 params = default_amst_params()
 params['n_workers'] = 12  # The default number of CPU cores is 8; set this to the number that is available
 
-amst_align(
-    raw_folder=raw_folder,
-    pre_alignment_folder=pre_alignment_folder,
-    target_folder=target_folder,
-    **params
-)
+# Due to the multiprocessing, in Windows, the following has to be inside a __name__ == '__main__' block
+# In Linux it doesn't matter, but it also does no harm
+if __name__ == '__main__':
+    amst_align(
+        raw_folder=raw_folder,
+        pre_alignment_folder=pre_alignment_folder,
+        target_folder=target_folder,
+        **params
+    )
