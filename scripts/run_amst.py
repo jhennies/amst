b37a7fbf7bf2e69a87a23007d62e4478fd635f92
jhennies <julianhennies@hotmail.de>
2022-02-17 14:06:32 +0100

    exposing median_radius in run_amst.py

--- scripts/run_amst.py
+++ scripts/run_amst.py
@@ -1,10 +1,4 @@
 
-"""
-This script only works for a windows installation.
-Also only very basic parameters are exposed, if you intend to run AMST with specialized parameter settings use the
-example_usage.py script
-"""
-
 from amst_linux.amst_main import amst_align, default_amst_params
 import argparse
 
@@ -15,6 +9,8 @@ parser.add_argument('--target_folder', type=str, default=None)
 parser.add_argument('--n_workers', type=int, default=8)
 parser.add_argument('--force_alignment', action='store_true',
                     help='Forces to align properly even if the crop from the raw data does not match the template size')
+parser.add_argument('--median_radius', type=int, default=7,
+                    help='Number of slices taken into account before and after the current slice for generation of the median smoothed template.')
 parser.add_argument('--verbose', action='store_true')
 
 args = parser.parse_args()
@@ -28,6 +24,7 @@ params = default_amst_params()
 params['n_workers'] = args.n_workers  # The default number of CPU cores is 8; set this to the number that is available
 params['verbose'] = args.verbose
 params['force_alignment'] = args.force_alignment
+params['median_radius'] = args.median_radius
 
 if __name__ == '__main__':
     amst_align(
@@ -35,3 +32,4 @@ if __name__ == '__main__':
         pre_alignment_folder=args.pre_alignment_folder,
         target_folder=args.target_folder,
         **params)
+
